0814fda75142c8cab4c797d3ecc0c2eab111365e
elblasco <blascovi@outlook.it>
2023-06-23 12:21:17 +0200

    Fixed nav graph and build.gradle.
    Init Tracker fragment.

---
+++ implementation/MangaCheck/ui/tracker/src/main/java/it/unitn/disi/lpsmt/g03/ui/tracker/TrackerFragment.kt
@@ -0,0 +1,41 @@
+package it.unitn.disi.lpsmt.g03.ui.tracker
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import it.unitn.disi.lpsmt.g03.ui.tracker.databinding.TrackerLayoutBinding
+
+class TrackerFragment: Fragment() {
+
+    private var _binding: TrackerLayoutBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = TrackerLayoutBinding.inflate(inflater, container, false)
+        // initializing variables of grid view with their ids.
+
+        binding.addButton.setOnClickListener {
+            //it.findNavController().navigate(R.id.action_libraryFragment_to_series_series)
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
